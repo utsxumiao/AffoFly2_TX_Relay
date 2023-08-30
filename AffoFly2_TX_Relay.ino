ddf399e1f47c26973de5d168a80dd53b93f5dac9
Eric Xu <utsxumiao@gmail.com>
2023-08-30 23:00:08 +1000

    Update AffoFly2_TX_Relay.ino

--- AffoFly2_TX_Relay.ino
+++ AffoFly2_TX_Relay.ino
@@ -2,15 +2,19 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <PPMReader.h>
 
 #define DEBUG
+#define SHOW_OUPUT
+#define SHOW_STATS
 
 #define LED_PIN           5
-#define CPPM_PIN          6
+#define CPPM_PIN          3
 #define NRF_CE_PIN        9
 #define NRF_CSN_PIN       10
-#define RADIO_PIPE              0xE8E8F0F0E1LL
-#define RADIO_CHANNEL           125
+#define RADIO_PIPE        0xE8E8F0F0E1LL
+#define RADIO_CHANNEL     125
+#define RADIO_TOKEN       "5734897589"
 #define CLOCK_MULTIPLIER  1       // 1 for 8MHZ, 2 for 16MHZ
 #define PPM_FRAME_LENGTH  20000
 #define PPM_PULSE_LENGTH  400
@@ -30,15 +34,15 @@ struct ControlData {
 
 RF24 radio(NRF_CE_PIN, NRF_CSN_PIN);
 ControlData controlData;
-uint16_t PPM[CHANNEL_COUNT];
+PPMReader ppm(CPPM_PIN, CHANNEL_COUNT);
 
 uint32_t currentTime = 0;
-uint16_t sendRadioInterval = 0;
+uint16_t sendRadioInterval = 50;
 uint32_t lastSendRadioMillis = 0;
-
+uint16_t showStatsInterval = 1000;
+uint32_t lastShowStatsMillis = 0;
 uint16_t loopCount = 0;
 uint16_t radioCount = 0;
-uint32_t ppmCount = 0;
 
 void setup() {
 #ifdef DEBUG
@@ -46,35 +50,24 @@ void setup() {
   Serial.println("System starting...");
 #endif
   Radio_init();
-  CPPM_init();
+  controlData.Token = RADIO_TOKEN;
 }
 
 void loop() {
   currentTime = millis();
   if (currentTime - lastSendRadioMillis >= sendRadioInterval) {
     lastSendRadioMillis = currentTime;
+    PPM_Read();
     Radio_output();
+    radioCount++;
   }
-  loopCount++;
-}
 
-void CPPM_init() {
-#ifdef DEBUG
-  Serial.print("Initialising CPPM......");
-#endif
-  pinMode(CPPM_PIN, OUTPUT);
-  PORTD = PORTD & ~B01000000;  //Set CPPM to low
-  cli();
-  TCCR1A = 0; // set entire TCCR1 register to 0
-  TCCR1B = 0;
-  OCR1A = 100;  // compare match register (not very important, sets the timeout for the first interrupt)
-  TCCR1B |= (1 << WGM12);  // turn on CTC mode
-  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
-  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
-  sei();
-#ifdef DEBUG
-  Serial.println("Done");
-#endif
+  if(currentTime - lastShowStatsMillis >= showStatsInterval) {
+    lastShowStatsMillis = currentTime;
+    Show_Stats();
+  }
+
+  loopCount++;
 }
 
 void Radio_init() {
@@ -94,54 +87,37 @@ void Radio_init() {
 #endif
 }
 
-void Radio_output() {
-  //uint32_t before = millis();
-  radio.write(&controlData, sizeof(ControlData));
-  //  uint32_t after = millis();
-  //  if(after - before > 10){
-  //    //TODO: once radio stuck, it will not revived by itself, intervention required.
-  //    Serial.print("Radio Slow! took: ");   Serial.println(after - before);
-  //  }
-  radioCount++;
+void PPM_Read() {
+  controlData.Throttle = ppm.latestValidChannelValue(1, 1000);
+  controlData.Yaw      = ppm.latestValidChannelValue(2, 1500);
+  controlData.Pitch    = ppm.latestValidChannelValue(3, 1500);
+  controlData.Roll     = ppm.latestValidChannelValue(4, 1500);
+  controlData.Aux1     = ppm.latestValidChannelValue(5, 1000);
+  controlData.Aux2     = ppm.latestValidChannelValue(6, 1000);
+  controlData.Aux3     = ppm.latestValidChannelValue(7, 1000);
+  controlData.Aux4     = ppm.latestValidChannelValue(8, 1000);
+
+#ifdef SHOW_OUPUT
+  Serial.print("Throttle: ");     Serial.print(controlData.Throttle);   Serial.print("    ");
+  Serial.print("Yaw: ");          Serial.print(controlData.Yaw);        Serial.print("    ");
+  Serial.print("Pitch: ");        Serial.print(controlData.Pitch);      Serial.print("    ");
+  Serial.print("Roll: ");         Serial.print(controlData.Roll);       Serial.print("    ");
+  Serial.print("Aux1: ");         Serial.print(controlData.Aux1);       Serial.print("    ");
+  Serial.print("Aux2: ");         Serial.print(controlData.Aux2);       Serial.print("    ");
+  Serial.print("Aux3: ");         Serial.print(controlData.Aux3);       Serial.print("    ");
+  Serial.print("Aux4: ");         Serial.print(controlData.Aux4);       Serial.print("    ");
+  Serial.println("");
+#endif
 }
 
-ISR(TIMER1_COMPA_vect) {
-  PPM[0] = controlData.Throttle;
-  PPM[1] = controlData.Yaw;
-  PPM[2] = controlData.Pitch;
-  PPM[3] = controlData.Roll;
-  PPM[4] = controlData.Aux1;
-  PPM[5] = controlData.Aux2;
-  PPM[6] = controlData.Aux3;
-  PPM[7] = controlData.Aux4;
+void Radio_output() {
+  radio.write(&controlData, sizeof(ControlData));
+}
 
-  static boolean state = true;
-  TCNT1 = 0;
-  if ( state ) {
-    //end pulse
-    PORTD = PORTD & ~B01000000; // turn pin 6 off.
-    OCR1A = PPM_PULSE_LENGTH * CLOCK_MULTIPLIER;
-    state = false;
-  } else {
-    //start pulse
-    static byte cur_chan_numb;
-    static unsigned int calc_rest;
-
-    PORTD = PORTD | B01000000; // turn pin 6 on.
-    state = true;
-
-    if (cur_chan_numb >= CHANNEL_COUNT) {
-      cur_chan_numb = 0;
-      calc_rest += PPM_PULSE_LENGTH;
-      OCR1A = (PPM_FRAME_LENGTH - calc_rest) * CLOCK_MULTIPLIER;
-      calc_rest = 0;
-    }
-    else {
-      OCR1A = (PPM[cur_chan_numb] - 10 - PPM_PULSE_LENGTH) * CLOCK_MULTIPLIER;
-      calc_rest += PPM[cur_chan_numb] - 10;
-      cur_chan_numb++;
-    }
-  }
-
-  ppmCount++;
+void Show_Stats() {
+  Serial.print("Loop: ");       Serial.print(loopCount);            Serial.print("    ");
+  Serial.print("Radio: ");      Serial.print(radioCount);           Serial.print("    ");
+  Serial.println();
+  loopCount = 0;
+  radioCount = 0;
 }
